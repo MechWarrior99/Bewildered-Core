dddc65f77c1fd2d90bd38b42a488d531d361ff9c
Mech <mechworrior20@gmail.com>
2021-01-31 10:02:42 -0800

    UIToolkit Popup window and Searchable Popup

---
+++ Scripts/Editor/AdvancedPopupWindow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor;
+
+namespace Bewildered.Editor
+{
+    internal class AdvancedPopupWindowContainer : EditorWindow
+    {
+
+    }
+
+    /// <summary>
+    /// Class used to implement a popup window using <see cref="VisualElement"/>s.
+    /// </summary>
+    public abstract class AdvancedPopupWindow
+    {
+        public EditorWindow EditorWindow
+        {
+            get;
+            internal set;
+        }
+
+        /// <summary>
+        /// Color used for the border of the window.
+        /// </summary>
+        protected static Color BorderColor
+        {
+            get { return EditorGUIUtility.isProSkin ? new Color(0.12f, 0.12f, 0.12f, 1.333f) : new Color(0.6f, 0.6f, 0.6f, 1.333f); }
+        }
+
+        public abstract VisualElement CreateContentElement();
+
+        public virtual void Show(Rect activeRect)
+        {
+            EditorWindow = ScriptableObject.CreateInstance<AdvancedPopupWindowContainer>();
+            EditorWindow.rootVisualElement.Add(CreateContentElement());
+            EditorWindow.rootVisualElement.style.borderTopColor = BorderColor;
+            EditorWindow.rootVisualElement.style.borderRightColor = BorderColor;
+            EditorWindow.rootVisualElement.style.borderBottomColor = BorderColor;
+            EditorWindow.rootVisualElement.style.borderLeftColor = BorderColor;
+            EditorWindow.rootVisualElement.style.borderTopWidth = 1;
+            EditorWindow.rootVisualElement.style.borderRightWidth = 1;
+            EditorWindow.rootVisualElement.style.borderBottomWidth = 1;
+            EditorWindow.rootVisualElement.style.borderLeftWidth = 1;
+            EditorWindow.ShowAsDropDown(GUIUtility.GUIToScreenRect(activeRect), GetWindowSize());
+        }
+
+        public void ShowDebugable(Rect activeRect)
+        {
+            EditorWindow = ScriptableObject.CreateInstance<AdvancedPopupWindowContainer>();
+            EditorWindow.rootVisualElement.Add(CreateContentElement());
+            EditorWindow.Show();
+        }
+
+        public virtual Vector2 GetWindowSize()
+        {
+            return new Vector2(200.0f, 200.0f);
+        }
+    }
+}
---
+++ Scripts/Editor/FilteredList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Bewildered.Editor
+{
+    /// <summary>
+    /// Represents a collection of items that can be filtered by a string value.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of elements in the <see cref="FilteredList{T}"/>.</typeparam>
+    public class FilteredList<T> : IEnumerable<FilteredList<T>.Entry>
+    {
+        public struct Entry
+        {
+            /// <summary>
+            /// Index of the item in the unfiltered collection.
+            /// </summary>
+            public int Index;
+
+            /// <summary>
+            /// The entry item.
+            /// </summary>
+            public T Item;
+        }
+
+        /// <summary>
+        /// The unfiltered items.
+        /// </summary>
+        public T[] AllItems { get; }
+
+        /// <summary>
+        /// The string used to filter items by.
+        /// </summary>
+        public string Filter { get; private set; }
+
+        /// <summary>
+        /// The list of items filtered by <see cref="Filter"/> with their index.
+        /// </summary>
+        public List<Entry> Entries { get; private set; } = new List<Entry>();
+
+        /// <summary>
+        /// The list of items filtered by <see cref="Filter"/>.
+        /// </summary>
+        public List<T> FilteredItems { get; private set; } = new List<T>();
+
+        /// <summary>
+        /// Function to get the string to filter by from an item.
+        /// </summary>
+        public Func<T, string> GetItemString { get; }
+
+        /// <summary>
+        /// Retrive an <see cref="Entry"/> in the filtered list by index.
+        /// </summary>
+        /// <param name="index">Index in the filtered list.</param>
+        public Entry this[int index]
+        {
+            get { return Entries[index]; }
+        }
+
+        /// <param name="items">The items to filter.</param>
+        /// <param name="getItemString"> Function to get the string to filter by from an item.</param>
+        public FilteredList(T[] items, Func<T, string> getItemString)
+        {
+            AllItems = items;
+            GetItemString = getItemString;
+            UpdateFilter("");
+        }
+
+        /// <summary>
+        /// Updates the <see cref="Filter"/>, and refilters <see cref="Entries"/> if there is achange in the filter value.
+        /// </summary>
+        /// <param name="filter">The new filter value.</param>
+        /// <returns><c>true</c> if <paramref name="filter"/> is different than the current <see cref="Filter"/>; otherwise, <c>false</c>.</returns>
+        public bool UpdateFilter(string filter)
+        {
+            if (filter == Filter)
+                return false;
+
+            Filter = filter;
+            Entries.Clear();
+            FilteredItems.Clear();
+
+            for (int i = 0; i < AllItems.Length; i++)
+            {
+                string itemString = GetItemString(AllItems[i]);
+                if (string.IsNullOrEmpty(Filter) || itemString.ToLower().Contains(Filter.ToLower()))
+                {
+                    Entry entry = new Entry()
+                    {
+                        Index = i,
+                        Item = AllItems[i]
+                    };
+
+                    if (string.Equals(itemString, filter, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        Entries.Insert(0, entry);
+                        FilteredItems.Insert(0, AllItems[i]);
+                    }
+                    else
+                    {
+                        Entries.Add(entry);
+                        FilteredItems.Add(AllItems[i]);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public IEnumerator<Entry> GetEnumerator()
+        {
+            return Entries.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Entries.GetEnumerator();
+        }
+    } 
+}
