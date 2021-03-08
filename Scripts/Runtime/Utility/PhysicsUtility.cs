362e1a2f3d5149dcf711c5d327f745d01e315ed5
Mech <mechworrior20@gmail.com>
2021-03-08 10:53:16 -0800

    Commented utility classes

--- Scripts/Runtime/Utility/PhysicsUtility.cs
+++ Scripts/Runtime/Utility/PhysicsUtility.cs
@@ -2,6 +2,9 @@
 
 namespace Bewildered
 {
+    /// <summary>
+    /// Various utility methods for helping work with physics related systems.
+    /// </summary>
     public static class PhysicsUtility
     {
         private static Camera _camera;
@@ -17,6 +20,10 @@ namespace Bewildered
             }
         }
 
+        /// <summary>
+        /// Casts a ray forward from the center of the screen of the main <see cref="Camera"/> in the scene, against all colliders in the scene.
+        /// </summary>
+        /// <returns><c>true</c> if ray intersects with a <see cref="Collider"/>; otherwise, <c>false</c>.</returns>
         public static bool FirstPersonRaycast()
         {
             return Physics.Raycast(SceneCamera.WorldToScreenPoint(_screenCenter), SceneCamera.transform.forward);
@@ -37,6 +44,14 @@ namespace Bewildered
             return Physics.Raycast(SceneCamera.WorldToScreenPoint(_screenCenter), SceneCamera.transform.forward, out hitInfo, maxDistance, layerMask);
         }
 
+        /// <summary>
+        /// Casts a ray forward from the center of the screen of the main <see cref="Camera"/> in the scene, against all colliders in the scene.
+        /// If no collisions occure by the specified max distance, it casts a ray directly down from the max distance point, returning the results from that ray instead.
+        /// </summary>
+        /// <param name="hitInfo">If <c>true</c> is returned <paramref name="hitInfo"/> will contain more information about where the closest collider was hit.</param>
+        /// <param name="maxDistance">The maximum distance the first ray should check for collisions.</param>
+        /// <param name="layerMask">The <see cref="LayerMask"/> that is used to selectivly ignore <see cref="Collider"/>s when casting a ray.</param>
+        /// <returns><c>true</c> if either of the rays intersect with a <see cref="Collider"/>; otherwise, false.</returns>
         public static bool FirstPersonGroundRaycast(out RaycastHit hitInfo, float maxDistance, int layerMask)
         {
             Vector3 viewportWorldPoint = SceneCamera.ViewportToWorldPoint(_screenCenter);
--- Scripts/Runtime/Utility/ReflectionUtility.cs
+++ Scripts/Runtime/Utility/ReflectionUtility.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Reflection;
 
 namespace Bewildered
@@ -8,6 +7,11 @@ namespace Bewildered
     {
         public const BindingFlags CommonFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
 
+        /// <summary>
+        /// Creates a delegate of the specified type to represent the specified stat method.
+        /// </summary>
+        /// <typeparam name="T">The type of delegate to create.</typeparam>
+        /// <returns>A delegate of the specified type that represents the specified method.</returns>
         public static T CreateDelegate<T>(this MethodInfo method) where T : Delegate
         {
             return Delegate.CreateDelegate(typeof(T), method) as T;
